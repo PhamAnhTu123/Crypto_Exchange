f1ae0d802cba67963f21fa8ffc2ee13410c14855
PhamAnhTu123 <tu.pham@nestsera.com>
2021-12-14 16:47:02 +0700

    init commit

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+require('babel-register');
+require('babel-polyfill');
+var HDWalletProvider = require("@truffle/hdwallet-provider")
+var mnemonic = "wolf predict tiger shock indicate aisle coconut retire release fish spin invest";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/58b5a2ffafbb4a13870980fb52714b6a")
+      },
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 21
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
